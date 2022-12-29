7217b76721de583bac45d0f5217e53cdba952020
Phillip Foo <pfoo360@gmail.com>
2022-12-29 05:04:59 -0500

    update base url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://filebase-kp0xe7r7q-pfoo360.vercel.app";
 
 export default axios.create({ baseURL: BASE_URL });
