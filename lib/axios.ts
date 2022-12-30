448dff3b2d92d3c878ca8cdd4c89cea7f7d026f7
Phillip Foo <pfoo360@gmail.com>
2022-12-30 00:52:43 -0500

    reverted baseURL; left a comment if i want to use localhost:3000 instead

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = (process.env.VERCEL_URL ?? "http://localhost:3000") as string;
+//const BASE_URL = "http://localhost:3000";
+
+const BASE_URL = process.env.VERCEL_URL as string;
 
 export default axios.create({ baseURL: BASE_URL });
