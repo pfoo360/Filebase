48b1c06a5160bf756fee5e37c03d2a932d1e8f12
Phillip Foo <pfoo360@gmail.com>
2022-12-30 00:38:56 -0500

    updated BASE URL with localhost:3000 has fallback option

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = process.env.VERCEL_URL as string;
+const BASE_URL = (process.env.VERCEL_URL ?? "http://localhost:3000") as string;
 
 export default axios.create({ baseURL: BASE_URL });
